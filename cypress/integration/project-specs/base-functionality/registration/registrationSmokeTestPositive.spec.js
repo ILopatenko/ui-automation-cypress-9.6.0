8cad3d37892d2f8c5ab4da644bc507826c37c429
Iurii Lopatenko <yalopatenko@gmail.com>
2022-06-23 21:01:39 -0700

    Registration Smoke Test Happy path

---
+++ cypress/integration/project-specs/base-functionality/registration/registrationSmokeTestPositive.spec.js
@@ -0,0 +1,66 @@
+import RegistrationPageDefaultPOM from '../../../../support/page-objects/before-login-poms/registrationPageDefaultPOM';
+import LocalHelper from '../../../../support/page-objects/localHelper';
+const registrationPageDefaultPOM = new RegistrationPageDefaultPOM();
+const localHelper = new LocalHelper();
+
+describe('Registration Page. Smoke Test Positive - main test suite', () => {
+  const validCredentials = {};
+  describe('PRECONDITIONS: load Registration Page', () => {
+    it('Load Registration Page', () => {
+      cy.visit('/user/register');
+    });
+  });
+  describe('SMOKE TEST HAPPY PATH', () => {
+    const newUserDataSet =
+      localHelper.generateUserDataForRegistration();
+    it('Enter valid email', () => {
+      registrationPageDefaultPOM
+        .emailInput()
+        .type(newUserDataSet.email);
+    });
+    it('Enter valid password', () => {
+      registrationPageDefaultPOM
+        .passwordInput()
+        .type(newUserDataSet.password);
+    });
+    it('Click submit button', () => {
+      registrationPageDefaultPOM.submitButton().click().wait(1000);
+    });
+    it('Enter Full Name', () => {
+      registrationPageDefaultPOM
+        .fullNameInputField()
+        .type(newUserDataSet.firstName)
+        .type(' ')
+        .type(newUserDataSet.lastName);
+    });
+    it('Enter Phone Number', () => {
+      registrationPageDefaultPOM
+        .phoneNumberInputField()
+        .type(newUserDataSet.phoneNumberUS);
+    });
+    it('Click submit button', () => {
+      registrationPageDefaultPOM.submitButton().click().wait(1000);
+    });
+    it('Enter a Company Name', () => {
+      registrationPageDefaultPOM
+        .companyNameInputField()
+        .type(newUserDataSet.companyName);
+    });
+    it('Enter a Primary Industry', () => {
+      registrationPageDefaultPOM
+        .primaryIndustryInputField()
+        .type(newUserDataSet.industry);
+    });
+    it('Click on Drop Down menu and select a random option', () => {
+      registrationPageDefaultPOM.companySizeDropDownButton().click();
+
+      registrationPageDefaultPOM.companySizeList().click().wait(1000);
+    });
+    it('Click submit button', () => {
+      registrationPageDefaultPOM.submitButton().click().wait(2000);
+    });
+    it('Click LOGO', () => {
+      registrationPageDefaultPOM.logoSpan().click().wait(1000);
+    });
+  });
+});
--- cypress/support/page-objects/localHelper.js
+++ cypress/support/page-objects/localHelper.js
@@ -8,9 +8,11 @@ export default class LocalHelper {
   //TOP BUTTONS
   topButtonsMainDiv = () => cy.get('div#top-menu').next();
   //MAIN BLOCKS ROOT DIV
-  mainBlocksRootDiv = () => cy.get('div#top-menu').parent().parent().next();
+  mainBlocksRootDiv = () =>
+    cy.get('div#top-menu').parent().parent().next();
   //Main Blocls selector (by number)
-  getMainBlockByNumber = (x) => this.mainBlocksRootDiv().children().eq(x);
+  getMainBlockByNumber = (x) =>
+    this.mainBlocksRootDiv().children().eq(x);
   topMenuLinks = ['Pricing', 'Industries', 'Support'];
   topButtons = [
     ['Log in', 'login'],
@@ -57,5 +59,223 @@ export default class LocalHelper {
           .go('back');
       });
     });
+  };
+
+  //
+  //
+  testData = {
+    registration: {
+      firstNames: [
+        'James',
+        'Robert',
+        'John',
+        'Michael',
+        'David',
+        'William',
+        'Richard',
+        'Joseph',
+        'Thomas',
+        'Charles',
+        'Christopher',
+        'Daniel',
+        'Matthew',
+        'Anthony',
+        'Mark',
+        'Donald',
+        'Steven',
+        'Paul',
+        'Andrew',
+        'Joshua',
+        'Mary',
+        'Patricia',
+        'Jennifer',
+        'Linda',
+        'Elizabeth',
+        'Barbara',
+        'Susan',
+        'Jessica',
+        'Sarah',
+        'Karen',
+        'Lisa',
+        'Nancy',
+        'Betty',
+        'Margaret',
+        'Sandra',
+        'Ashley',
+        'Kimberly',
+        'Emily',
+        'Donna',
+        'Michelle',
+      ],
+      lastNames: [
+        'Johnson',
+        'Williams',
+        'Brown',
+        'Jones',
+        'Garcia',
+        'Miller',
+        'Davis',
+        'Rodriguez',
+        'Martinez',
+        'Hernandez',
+        'Lopez',
+        'Gonzales',
+        'Wilson',
+        'Anderson',
+        'Thomas',
+        'Taylor',
+        'Moore',
+        'Jackson',
+        'Martin',
+        'Lee',
+        'Perez',
+        'Thompson',
+        'White',
+        'Harris',
+        'Sanchez',
+        'Clark',
+        'Ramirez',
+        'Lewis',
+        'Robinson',
+        'Walker',
+        'Young',
+        'Allen',
+        'King',
+        'Wright',
+        'Scott',
+        'Torres',
+        'Nguyen',
+        'Hill',
+        'Flores',
+      ],
+      alphabetLowerCase: 'abcdefghijklmnopqrstuvwxyz',
+      alphabetUpperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      numbers: '0123456789',
+      symbolsSet1: '`~!@#$%^&*()_-+=|{}[];:",.<>/?',
+      symbolsEmail: '!#$%&*_',
+      emailDomains: [
+        'outlook',
+        'gmail',
+        'yahoo',
+        'inbox',
+        'icloud',
+        'inbox',
+        'mail',
+        'aol',
+      ],
+      domains: ['ru', 'net', 'com', 'us', 'io', 'fr'],
+      industries: [
+        'Health Care and Virtual Medicine',
+        'Accommodation and Food Services',
+        'Arts, Entertainment, and Crafts',
+        'Personal Trainers (online)',
+        'Site building and web design',
+        'Local Auto Repairs',
+        'Secondhand (Online) Stores',
+        'Pet services',
+        'Moving',
+        'Delivery service',
+      ],
+    },
+  };
+  //
+  //
+
+  //
+  //
+  //
+  //REGISTRATION
+  //
+  getRandomElementFromGivenSetDefault1 = (set, length = 1) => {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += set[Math.floor(Math.random() * set.length)];
+    }
+    return result;
+  };
+  getRandomBetween = (a = 1, b = 9) =>
+    a + Math.floor(Math.random() * b);
+
+  generatePassword = (n) => {
+    return (
+      this.getRandomElementFromGivenSetDefault1(
+        this.testData.registration.alphabetLowerCase,
+        this.getRandomBetween(2, n)
+      ) +
+      this.getRandomElementFromGivenSetDefault1(
+        this.testData.registration.symbolsSet1,
+        this.getRandomBetween(2, n)
+      ) +
+      this.getRandomElementFromGivenSetDefault1(
+        this.testData.registration.alphabetUpperCase,
+        this.getRandomBetween(2, n)
+      ) +
+      this.getRandomElementFromGivenSetDefault1(
+        this.testData.registration.numbers,
+        this.getRandomBetween(2, n)
+      )
+    );
+  };
+  generateEmailBasedOnFullName1 = (
+    firstName,
+    lastName,
+    length = 3
+  ) => {
+    const emailDomain = this.getRandomElementFromGivenSetDefault1(
+      this.testData.registration.emailDomains
+    );
+    const domain = this.getRandomElementFromGivenSetDefault1(
+      this.testData.registration.domains
+    );
+
+    let capitalLetters = '';
+    let symbols = '';
+    let numbers = '';
+    for (let i = 0; i < length; i++) {
+      capitalLetters += this.getRandomElementFromGivenSetDefault1(
+        this.testData.registration.alphabetLowerCase
+      ).toUpperCase();
+      symbols += this.getRandomElementFromGivenSetDefault1(
+        this.testData.registration.symbolsEmail
+      );
+      numbers += `${Math.floor(Math.random() * 10)}`;
+    }
+    const email = `${firstName.toLowerCase()}_${capitalLetters}_${symbols}_${numbers}_${lastName.toLowerCase()}@${emailDomain}.${domain}`;
+    return email;
+  };
+
+  generateUserDataForRegistration = () => {
+    const firstName = this.getRandomElementFromGivenSetDefault1(
+      this.testData.registration.firstNames
+    );
+    const lastName = this.getRandomElementFromGivenSetDefault1(
+      this.testData.registration.lastNames
+    );
+    const symbols = Math.floor(Math.random() * 5) + 1;
+    const password = this.generatePassword(3);
+    const phoneNumberUS = this.getRandomElementFromGivenSetDefault1(
+      this.testData.registration.numbers,
+      10
+    );
+    const companyName = `${firstName}'s company LLC`;
+    const industry = this.getRandomElementFromGivenSetDefault1(
+      this.testData.registration.industries
+    );
+    const companySize = this.getRandomBetween(1, 100) % 4;
+    const email = this.generateEmailBasedOnFullName1(
+      firstName,
+      lastName,
+      symbols
+    );
+    return {
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumberUS,
+      companyName,
+      industry,
+      companySize,
+    };
   };
 }

2a59d1a0f329ef5f5834829ad599cce9fd221b67
Iurii Lopatenko <52218759+ILopatenko@users.noreply.github.com>
2022-06-23 21:05:11 -0700

    Registration Smoke Test Happy path (#6)

--- cypress/support/page-objects/before-login-poms/registrationPageDefaultPOM.js
+++ cypress/support/page-objects/before-login-poms/registrationPageDefaultPOM.js
@@ -6,5 +6,21 @@ export default class RegistrationPageDefaultPOM {
   mainForm = () => cy.get('form');
   modalCloseButton = () => cy.get('svg[data-testid="CloseIcon"]');
   modalMainDiv = () => cy.get('div[data-test="sentinelStart"]');
-  modalBlockWithData = () => this.modalMainDiv().next().children('div').last();
+  modalBlockWithData = () =>
+    this.modalMainDiv().next().children('div').last();
+
+  //for functional
+  emailInput = () => cy.get('input#email');
+  passwordInput = () => cy.get('input#password');
+  submitButton = () => cy.get('button[type="submit"]');
+  fullNameInputField = () => cy.get('input#fullName');
+  phoneNumberInputField = () => cy.get('input#phone_number');
+  companyNameInputField = () => cy.get('input#companyName');
+  primaryIndustryInputField = () => cy.get('input#primaryIndustry');
+  companySizeDropDownButton = () =>
+    cy.get('svg[data-testid="ArrowDropDownIcon"]');
+  companySizeList = () =>
+    cy
+      .get('div[role="presentation"]>div>ul>li')
+      .eq(Math.floor(Math.random() * 20) % 3);
 }
